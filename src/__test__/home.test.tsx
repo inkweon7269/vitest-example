18260f5969fd901f62de0685bae914d6e8968d4d
inkweon7269 <inkweon7269@gmail.com>
2024-08-04 20:34:07 +0900

    이전 코드로 복구

--- src/__test__/home.test.tsx
+++ src/__test__/home.test.tsx
@@ -6,7 +6,7 @@ import {renderWithProviders} from "@/utils/test/render";
 describe("LoginForm", () => {
     it('테스트 결과를 확인합니다.', async () => {
         renderWithProviders(<Home />);
-        const text = await screen.findByText('Vitest Example!!!');
+        const text = await screen.findByText('Vitest Example');
         expect(text).toBeInTheDocument();
     });
 });
