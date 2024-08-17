79f4c6396f0bcd199739856325a9a39e367a6edb
inkweon7269 <inkweon7269@gmail.com>
2024-08-18 00:54:53 +0900

    색상 변경

--- src/components/atoms/Label.tsx
+++ src/components/atoms/Label.tsx
@@ -5,7 +5,7 @@ export interface ILabelProps {
 
 const Label = ({ children, htmlFor }: ILabelProps) => {
   return (
-    <label htmlFor={htmlFor} style={{ color: "blue" }}>
+    <label htmlFor={htmlFor} style={{ color: "yellow" }}>
       {children}
     </label>
   );
